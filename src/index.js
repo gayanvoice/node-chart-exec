5fef7efe74cd0047e4a45994ada574c5d4514a15
GitHub Name <email@yadex.com>
2021-05-26 07:19:56 +0530

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,61 +2,88 @@ const core = require('@actions/core');
 const {ChartJSNodeCanvas} = require('chartjs-node-canvas');
 const file = require('./file/file');
 let Index = function () {
-    let main = async function (commandModel) {
-        core.info(`Node Chart Exec Start`);
-        let configType = commandModel.type;
-        let configHeight = commandModel.height;
-        let configWidth = commandModel.width;
-        const configLabels = JSON.parse(commandModel.labels);
-        const configDataset = JSON.parse(commandModel.dataset);
-        let configOutputFile = commandModel.outputFile;
-        if(configType === '' || configType === undefined){
-            configType = 'line';
-        }
-        if(configHeight === '' || configHeight === undefined){
-            configHeight = 400;
-        }
-        if(configWidth === '' || configWidth === undefined){
-            configWidth = 400;
-        }
-        if(configOutputFile === '' || configOutputFile === undefined){
-            configWidth = 'output.png';
-        }
-        core.info(`Type ${configType}`);
-        core.info(`Labels ${JSON.stringify(configLabels)}`);
-        core.info(`Output ${configOutputFile}`);
-        const chartJSNodeCanvas = new ChartJSNodeCanvas({width: configWidth, height: configHeight});
-        let datasets = [];
-        let numberOfDatasets = 1;
-        for (const dataset of configDataset) {
-            datasets.push({
-                label: dataset.label,
-                data: dataset.data,
-                backgroundColor: dataset.backgroundColor,
-                borderColor: dataset.borderColor
-            });
-            core.info(`Dataset [${numberOfDatasets}/${configDataset.length}]`);
-            core.info(`label ${dataset.label}`);
-            core.info(`data ${dataset.data}`);
-            core.info(`background color ${dataset.backgroundColor}`);
-            core.info(`border color-${dataset.borderColor}`);
-            numberOfDatasets++;
-        }
-        const configuration = {
-            type: configType,
-            data: {
-                labels: configLabels,
-                datasets: datasets
+    let isValidInput = function (input) {
+        return !(input === '' || input === undefined);
+    }
+    let isValidJson = function (json) {
+        if(isValidInput(json)){
+            try {
+                JSON.parse(json)
+                return true;
+            } catch (error) {
+                return false;
             }
-        };
-        try {
-            const image = await chartJSNodeCanvas.renderToBuffer(configuration);
-            await file.createImage(configOutputFile, image);
-            core.info(`Node Chart Exec Complete`);
-        } catch (error) {
-            core.info(`Node Chart Exec Failed`);
-            core.info(`Error ${error}`);
+        } else {
+            return false;
         }
+    }
+    let main = async function (commandModel) {
+        core.info(`Node Chart Exec Started`);
+        if(isValidInput(commandModel.type)){
+            if(isValidInput(commandModel.height)){
+                if(isValidInput(commandModel.width)){
+                    if(isValidInput(commandModel.outputFile)){
+                        if(isValidJson(commandModel.labels)){
+                            if(isValidJson(commandModel.dataset)){
+                                core.info(`All inputs are validated`);
+                                const configType = commandModel.type;
+                                const configHeight = commandModel.height;
+                                const configWidth = commandModel.width;
+                                const configOutputFile = commandModel.outputFile;
+                                const configLabels = JSON.parse(commandModel.labels);
+                                const configDataset = JSON.parse(commandModel.dataset);
+                                const chartJSNodeCanvas = new ChartJSNodeCanvas({width: configWidth, height: configHeight});
+                                core.info(`Canvas width=${configWidth} & height=${configHeight}`);
+                                core.info(`Chart type=${configType} labels=${JSON.stringify(configLabels)} output=${configOutputFile}`);
+                                let datasets = [];
+                                let numberOfDatasets = 1;
+                                for (const dataset of configDataset) {
+                                    datasets.push({
+                                        label: dataset.label,
+                                        data: dataset.data,
+                                        backgroundColor: dataset.backgroundColor,
+                                        borderColor: dataset.borderColor
+                                    });
+                                    core.info(`Dataset [${numberOfDatasets}/${configDataset.length}] 
+                                    label=${dataset.label} 
+                                    data=${dataset.data} 
+                                    background-color=${dataset.backgroundColor} 
+                                    border-color=${dataset.borderColor}`);
+                                    numberOfDatasets++;
+                                }
+                                const configuration = {
+                                    type: configType,
+                                    data: {
+                                        labels: configLabels,
+                                        datasets: datasets
+                                    }
+                                };
+                                try {
+                                    const image = await chartJSNodeCanvas.renderToBuffer(configuration);
+                                    await file.createImage(configOutputFile, image);
+                                } catch (error) {
+                                    core.info(`Node Chart Exec Failed`);
+                                    core.info(`Error ${error}`);
+                                }
+                            } else {
+                                core.info(`Please enter a valid dataset.`)
+                            }
+                        } else {
+                            core.info(`Please enter valid labels.`)
+                        }
+                    } else {
+                        core.info(`Please enter a valid output file.`)
+                    }
+                } else {
+                    core.info(`Please enter a valid width.`)
+                }
+            } else {
+                core.info(`Please enter a valid height.`)
+            }
+        } else {
+            core.info(`Please enter a valid type.`)
+        }
+        core.info(`Node Chart Exec Completed`);
     }
     return {
         main: main,
